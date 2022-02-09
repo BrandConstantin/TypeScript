302b9d01de3180254152dfbce81d07d2fbcfbbf0
BrandConstantin <constantin.brindu@gmail.com>
2022-02-09 07:28:10 +0100

    tipos especiales de typescript

---
+++ 03_02_tiposEspeciales_TypeScript/app.js
@@ -0,0 +1,13 @@
+// any
+let cualquiera: any = "es un string ahora";
+cualquiera = 5;
+cualquiera = false;
+
+// null
+let nada: null = null;
+
+// undefined
+let indefinido: undefined = undefined;
+
+// void
+let nada2: void = undefined;
