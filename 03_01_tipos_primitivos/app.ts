08d6a3e58f434ffddd104a7f99214b30e12f2bb5
BrandConstantin <constantin.brindu@gmail.com>
2022-02-09 07:27:41 +0100

    tipos primitivos

---
+++ 03_01_tipos_primitivos/app.ts
@@ -0,0 +1,11 @@
+// boolean
+let logico = false;
+let esReal: boolean = true;
+
+// numericos
+let uno = 1;
+let dos: number = 2.4;
+let numeroLargo: number = 1_345_432_989;
+
+// string
+let cadena = 'Hola Mundo';
