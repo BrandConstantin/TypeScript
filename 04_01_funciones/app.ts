c5792f43cc6b2a816eb7010cd88f46b4590e279d
BrandConstantin <constantin.brindu@gmail.com>
2022-02-11 07:01:22 +0100

    funciones

---
+++ 04_01_funciones/app.ts
@@ -0,0 +1,7 @@
+function sumar(num1: number, num2: number): number{
+    let suma = num1 + num2;
+    console.log(`${num1} + ${num2} = ${suma}`);
+    return suma;
+}
+
+let sumado: number = sumar(3, 4);
