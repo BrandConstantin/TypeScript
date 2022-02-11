9f93bf8d06ba56cef922fbb5a9244c2fb614d12d
BrandConstantin <constantin.brindu@gmail.com>
2022-02-11 06:52:04 +0100

    arrays, tuplas, objetos

---
+++ 03_03_arrays_tuplas_objetos/app.js
@@ -0,0 +1,10 @@
+// array
+var a = [1, 2, 3, 4, 5];
+var b = ["lunes", "martes", "miercoles"];
+var c = ["febrero", 2, null];
+// tuplas
+var db = [1, "Daniel", 23, false];
+// objetos
+var persona = {
+    nombre: "Pepe", edad: 32, pais: "Colombia", fecha_nac: new Date(1990, 4, 12), laboral: { empresa: "Empresa", sector: "Informática" }
+};
