5ef3177e78d3feef5480b7204fb3b2f034581651
BrandConstantin <constantin.brindu@gmail.com>
2022-02-11 07:19:27 +0100

    funciones tipadas

---
+++ 04_02_funciones_tipadas/app.js
@@ -0,0 +1,10 @@
+var restar = function (m, n) {
+    var resta = m - n;
+    console.log("".concat(m, " - ").concat(n, " = ").concat(resta));
+    return resta;
+};
+// v2
+var multiplicar;
+multiplicar = function (m, n) { return m * n; };
+var restado = restar(4, 1);
+var multiplicado = multiplicar(2, 5);
