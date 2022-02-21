4b56edc3b217d26bf91068189f86781993513caa
BrandConstantin <constantin.brindu@gmail.com>
2022-02-21 07:29:09 +0100

    tipos alias

---
+++ 06_01_tipos_alias/app.ts
@@ -0,0 +1,13 @@
+type Numero = number;
+let n: Numero = 5;
+
+type LoginCredentials = {
+    username: string,
+    password: string,
+    remember?: boolean
+}
+function login(data: LoginCredentials){ }
+login({
+    username: 'admin',
+    password: 'admin'
+})
