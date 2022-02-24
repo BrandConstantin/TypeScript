80089f614486f7c65bca949d43f95aaae15dc61e
BrandConstantin <constantin.brindu@gmail.com>
2022-02-24 07:32:33 +0100

    tipos enumerados con valores computados

---
+++ 12_03_enumerados_valores_computados/app.ts
@@ -0,0 +1,9 @@
+enum Permisos{ // los números representa el permiso en este ejemplo
+    // método 1 que se complica si existe muchos permisos
+    // EscribirMensajes = 1, Reaccionar = 2, EnviarEnlaces = 4, ConectarseCanalesDeVoz = 8
+    // métodos 2
+    EscribirMensajes = 1,
+    Reaccionar = EscribirMensajes + 2,
+    EnviarEnlaces = Reaccionar + 2,
+    ConectarseCanalesDeVoz = EnviarEnlaces + 2
+}
